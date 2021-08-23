c0ec90222659ee41e3f5c4d24e3b23fb71110133
PASSAGYR69 <84930210+PASSAGYR69@users.noreply.github.com>
2021-08-23 19:55:33 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min (arr) {
+  if (arr == undefined || arr.length == 0) {return 0};
+  let num = arr[0];
 
-exports.min = function min (array) {
-  return 0;
+  for(i = 1; i < arr.length; i++) {
+    if (arr[i] < num) {num = arr[i]}
+  }
+    return num;
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (arr == undefined || arr.length == 0) {return 0};
+  let num = arr[0];
+  
+  for(i = 1; i < arr.length; i++) {
+    if (arr[i] > num) {num = arr[i]}
+    }
+    return num;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr == undefined || arr.length == 0) {return 0};
+  let sum = 0;
+  for(i=0; i < arr.length; i++) {
+      sum += arr[i]
+  }
+  return sum / arr.length
 }
